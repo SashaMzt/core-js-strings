2f94924c1843d053e3d6def36c6eee652e57b5a1
Sasha <alex.mzt.147@gmail.com>
2024-11-16 11:52:21 +0200

    feat: task getStringLength

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,11 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(str) {
+  if (typeof str !== 'string') {
+    return 0;
+  }
+  return str.length;
 }
 
 /**
